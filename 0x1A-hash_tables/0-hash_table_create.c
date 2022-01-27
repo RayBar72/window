a90157f47a73eb028a99858c765db4a64797c1ef
RayBar72 <3963@holbertonschool.com>
2022-01-27 18:45:04 -0300

    Respaldo 220127

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function that creates a hash table
+ * @size: size of the array 
+ * Return: pointer to new hash table or NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+    hash_table_t *table = NULL;
+    unsigned long int i = 0;
+
+    if (size < 1)
+        return (NULL);
+    table = malloc(sizeof(*table));
+    if (!table)
+        return (NULL);
+    table->array = malloc(sizeof(table->array) * size);
+    if (!table->array)
+    {
+        free(table);
+        return (NULL);
+    }
+    table->size = size;
+    for (i = 0; i < size; i++)
+        table->array[i] = NULL;
+    return (table);
+}
