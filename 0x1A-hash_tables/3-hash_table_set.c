31631ff43d185078c767f8b16b65b8e05c1773b8
RayBar72 <3963@holbertonschool.com>
2022-01-28 13:54:48 -0300

    Error de paréntesis

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,45 @@
 #include "hash_tables.h"
 
+/**
+ * search_node - looks at hash ind. post for the existence
+ * of node
+ * 
+ * @head: pointer to positiones
+ * @key: of the node
+ * @Return - pointer or string
+ */
+static hash_node_t *search_head(hash_node_t *head, char *key)
+{
+	while ((head))
+	{
+		if (strcmp(head->key, key))
+			head = head->next;
+		else
+			return (head);		
+	}
+	return (NULL);
+}
+
+
+/**
+ * set_node - Set the node object
+ * @head: pointer to pointer
+ * @key: key of the node
+ * @value: value of node var
+ * Return: pointer if success or NULL
+ */
+static hash_node_t *node_setting(hash_node_t **head, char *key, char *value)
+{
+	hash_node_t *new = malloc(sizeof(new));
+
+	if (!new)
+		return (NULL);
+	new->key = key;
+	new->value = value;
+	new->key = *head;
+	*head = new;
+	return (new);
+}
 
 
 
@@ -12,44 +52,36 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index = 0, counter = 0;
-    hash_node_t *new = NULL, *recorre = NULL;
+	unsigned long int index = 0, counter = 0;
+	hash_node_t *new = NULL, *search = NULL;
+	char *var = NULL, key_resp = NULL;
 
-    if (!ht || !key || !value || value[0] == '\0')
-        return(0);
-    /*Generando índice*/
-    index = key_index((unsigned char *)key, ht->size);
-
-    /*Buscando si nodo ocupado*/
-    if (ht->array[index] == NULL)
-    {
-        /*Null: iguala valores del nodo*/
-        ht->array[index]->key = strdup(key);
-            if (!ht->array[index]->key)
-                return (0);
-        ht->array[index]->value = strdup(value);
-        ht->array[index]->next = NULL;
-        return (1); 
-    }
-    else
-    {
-        /*Crea malloc*/
-        new = malloc(sizeof(new));
-        if (!new)
-            return (0);
-        new->key = key;
-        new->value = NULL;
-        recorre = ht->array[index];
-        while (recorre)
-        {
-            recorre->next = 
-        }
-        
-
-        
-    }
-
-
-
-
+	if (!ht || !key || !value || value[0] == '\0')
+		return(0);
+	/*valor a introducir si lugar en lista está vacío*/
+	var = strdup(value);
+	if (!value)
+		return (0);
+	/*Generando índice*/
+	index = key_index((unsigned char *)key, ht->size);
+	/*Buscando si nodo ocupado*/
+	search = search_head(ht->array[index], (char *)key);
+	if (search)
+		search->value = var;
+	else
+	{
+		key_resp = strdup(key);
+		if (!key_resp)
+		{
+			free(var);
+			return (0);
+		}
+		search = node_setting(&(ht->array[index]), key_resp, var);
+		if (!search)
+		{
+			free(key_resp);
+			free(var);
+			return (0);
+		}
+	}
 }
