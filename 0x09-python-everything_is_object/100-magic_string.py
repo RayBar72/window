a29ac269b3a125dcd8a990f848e2578917bab4b3
RayBar72 <3963@holbertonschool.com>
2022-02-04 14:57:41 -0300

    Proyecto Circle

--- 0x09-python-everything_is_object/100-magic_string.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def magic_string(contador = []):
-    contador.append(1)
-    return ", ".join(["BestSchool"] * len(contador))
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Unittest eviroment for Base Class"""
+import unittest
+import os
+
+
